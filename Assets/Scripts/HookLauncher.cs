6666ba38e83273c94cfa70889fa2065e861a799c
ZacharyRMcKee <zmckee@u.northwestern.edu>
2019-02-05 05:34:41 -0600

    Prototype for getting right stick input for grappling hook aim

--- Assets/Scripts/HookLauncher.cs
+++ Assets/Scripts/HookLauncher.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class HookLauncher : MonoBehaviour
 {
+    public const float LaunchCooldown = 0.5f;
+    public const float LaunchVelocity = 3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float Horizontal = Input.GetAxis("Right Horizontal");
+        float Vertical = Input.GetAxis("Right Vertical");
+        Debug.Log(Vertical);
+        float angle = Mathf.Atan2(Vertical, Horizontal);
+
+        GameObject Hook = FindObjectOfType<GrapplingHook>().gameObject;
+        var HookBody = Hook.GetComponent<Rigidbody2D>();
+        HookBody.MoveRotation(Mathf.Rad2Deg * angle - 135);
     }
 }
