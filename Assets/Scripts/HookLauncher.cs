af9417d7989808a7a5c38707aa3b895686166d9e
ZacharyRMcKee <zmckee@u.northwestern.edu>
2019-02-07 03:36:13 -0600

    Add Fire method, as well as helper methods for aiming

--- Assets/Scripts/HookLauncher.cs
+++ Assets/Scripts/HookLauncher.cs
@@ -6,10 +6,12 @@ public class HookLauncher : MonoBehaviour
 {
     public const float LaunchCooldown = 0.5f;
     public const float LaunchVelocity = 3f;
+    private static Object HookPrefab;
+    float lastShotTime = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        HookPrefab = Resources.Load("GrapplingHook");
     }
 
     // Update is called once per frame
@@ -17,21 +19,45 @@ public class HookLauncher : MonoBehaviour
     {
         float Horizontal = Input.GetAxis("Right Horizontal");
         float Vertical = Input.GetAxis("Right Vertical");
-        if (Mathf.Abs(Horizontal) + Mathf.Abs(Vertical) > 0.3f)
+        if (GameInput.SignificantStickInput(Vertical,Horizontal))
         {
-            float angle = Mathf.Atan2(Vertical, Horizontal) * Mathf.Rad2Deg;
-            DisplayAimReticle(angle);
+            float AimAngle = CalculateAimAngle(Vertical, Horizontal);
+            DisplayAimReticle(AimAngle);
         }
         else
         {
-            Color color = gameObject.GetComponent<SpriteRenderer>().color;
-            color.a = 0;
-            gameObject.GetComponent<SpriteRenderer>().color = color;
+            HideAimReticle();
         }
     }
-    void DisplayAimReticle(float angle)
+    private float CalculateAimAngle(float Vertical, float Horizontal)
+    {
+        return Mathf.Atan2(Vertical, Horizontal) * Mathf.Rad2Deg;
+    }
+    private void HideAimReticle()
+    {
+        Color color = gameObject.GetComponent<SpriteRenderer>().color;
+        color.a = 0;
+        gameObject.GetComponent<SpriteRenderer>().color = color;
+    }
+    private void DisplayAimReticle(float angle)
     {
         gameObject.GetComponent<SpriteRenderer>().color = Color.green;
         transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    void Fire()
+    {
+        float time = Time.time;
+        if (time > lastShotTime + LaunchCooldown)
+        {
+            lastShotTime = time;
+            spawnHook(transform.position + transform.up * 10f,
+                transform.rotation,
+                transform.up * LaunchVelocity);
+
+        }
+    }
+    void spawnHook(Vector2 position, Quaternion rotation, Vector2 velocity)
+    {
+
     }
 }
