67696c1425938fcc58d53cfb1236211829bcb012
ZacharyRMcKee <zmckee@u.northwestern.edu>
2019-02-08 07:19:54 -0600

    Implemented very basic, broken functionality for the grappling hook itself

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-    }
+        Debug.Log("Collision detected");
+        if (collision.gameObject.tag == "Outer Platform" ||
+            collision.gameObject.tag == "Inner Platform")
+        {
+            Outer_Movement ShooterMovement = transform.parent.
+                gameObject.GetComponent<Outer_Movement>();
+            ShooterMovement.DetachFromPlatform();
+            PlantHook();
+            DragPlayerToHook();
+        }
 
-    // Update is called once per frame
-    void Update()
+    }
+    void PlantHook()
     {
-        
+        gameObject.GetComponent<Rigidbody2D>().constraints =
+            RigidbodyConstraints2D.FreezeAll;
+    }
+    // TODO: Clean this up
+    void DragPlayerToHook()
+    {
+        GameObject Player = transform.parent.gameObject;
+        Vector3 HookPosition = transform.position;
+        Vector3 PlayerPosition = Player.transform.position;
+        Vector3 PlayerVelocity = Vector3.Normalize(HookPosition - PlayerPosition) *
+                                    20f;
+        Debug.Log(PlayerVelocity);
+        Player.GetComponent<Rigidbody2D>().velocity = PlayerVelocity;
     }
 }
