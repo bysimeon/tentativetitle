a330a24ea47c8f6b9367b80538ce72f731e71c6b
ZacharyRMcKee <zmckee@u.northwestern.edu>
2019-02-15 02:42:36 -0600

    Makes player's name flash red when hit

--- Assets/Scripts/Damage_Player.cs
+++ Assets/Scripts/Damage_Player.cs
@@ -20,12 +20,19 @@ public class Damage_Player : MonoBehaviour
     public GameObject p2_heart2;
     public GameObject p2_heart3;
 
+
+    private Color flashColor;
+    private Color textColor;
+
+
     public Text win_text;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        flashColor = Color.red;
+        textColor = GameObject.FindWithTag("Player1Text").
+            GetComponent<Text>().color;
     }
 
     // Update is called once per frame
@@ -41,10 +48,28 @@ public class Damage_Player : MonoBehaviour
             Destroy(player2.gameObject);
         }
     }
+    IEnumerator Flash(GameObject damagedPlayer)
+    {
+        string playerTag = damagedPlayer.tag;
+        GameObject text = GameObject.FindWithTag(playerTag + "Text");
+        for (float f = 0f; f < 1.15f; f += 0.15f)
+        {
+            // check to see if the Player's destroyed
+            if(damagedPlayer)
+            {
+                Color currentColor = text.GetComponent<Text>().color;
+                text.GetComponent<Text>().color = currentColor.Equals(textColor) ?
+                    flashColor :
+                    textColor;
+            }
+            yield return new WaitForSeconds(.15f);
+        }
+    }
 
     public void IncrementP1()
     {
         player1_count++;
+        StartCoroutine("Flash",GameObject.FindWithTag("Player1"));
         if(player1_count == 1)
         {
             Destroy(p1_heart3);
@@ -62,6 +87,7 @@ public class Damage_Player : MonoBehaviour
 
     public void IncrementP2()
     {
+        StartCoroutine("Flash", GameObject.FindWithTag("Player2"));
         player2_count++;
         if (player2_count == 1)
         {
