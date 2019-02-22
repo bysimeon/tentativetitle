2b41b80de408529bf4dfe7f578ee4d62e29af3ae
Zachary McKee <zmckee@u.northwestern.edu>
2019-02-22 01:05:13 -0600

    Create PlayerColors class to help make managing colors easier

---
+++ Assets/Scripts/PlayerColors.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColors
+{
+    Color player1 = new Color(58, 255, 120);
+    Color player2= new Color(58, 255, 120);
+    Color player3r = new Color(226, 110, 223);
+    Color player4= new Color(58, 255, 120);
+}
